1f9ffe364dd9abda051b5906a084be84d4e7f8cc
Shayne van Asperen <shaynevanasperen@gmail.com>
2015-12-05 21:11:47 +0000

    Update SymbolSource duplicate package error

--- Data.Operations/.Tasks.ps1
+++ Data.Operations/.Tasks.ps1
@@ -1,6 +1,6 @@
 param(
 	$prereleaseVersion = (property prereleaseVersion "beta-1"),
-	$ignoreNugetPushErrors = (property ignoreNugetPushErrors "exists in compilation with different binary hash") #SymbolSource duplicate package error
+	$ignoreNugetPushErrors = (property ignoreNugetPushErrors "The underlying connection was closed: An unexpected error occurred on a receive.;exists in compilation with different binary hash") #SymbolSource duplicate package error
 )
 
 Include-PluginScripts
