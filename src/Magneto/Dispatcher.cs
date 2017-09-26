4f5cdb2aefec3f0f86b7117c96d0e54c53227864
Joseph Woodward <joseph.woodward@xeuse.com>
2017-09-26 21:36:54 +0100

    Add rider to .gitignore, expression bodied functions and pattern matching

--- src/Magneto/Dispatcher.cs
+++ src/Magneto/Dispatcher.cs
@@ -20,68 +20,42 @@ namespace Magneto
 		protected IInvoker Invoker { get; }
 
 		protected virtual TContext GetContext<TContext>()
-		{
-			return (TContext)ServiceProvider.GetService(typeof(TContext));
-		}
+			=> (TContext)ServiceProvider.GetService(typeof(TContext));
 
 		public virtual TResult Query<TContext, TResult>(ISyncQuery<TContext, TResult> query)
-		{
-			return Invoker.Query(query, GetContext<TContext>());
-		}
+			=> Invoker.Query(query, GetContext<TContext>());
 
 		public virtual Task<TResult> QueryAsync<TContext, TResult>(IAsyncQuery<TContext, TResult> query)
-		{
-			return Invoker.QueryAsync(query, GetContext<TContext>());
-		}
+			=> Invoker.QueryAsync(query, GetContext<TContext>());
 
 		public virtual TResult Query<TContext, TResult, TCacheEntryOptions>(ISyncCachedQuery<TContext, TCacheEntryOptions, TResult> query, CacheOption cacheOption = CacheOption.Default)
-		{
-			return Invoker.Query(query, GetContext<TContext>(), cacheOption);
-		}
+			=> Invoker.Query(query, GetContext<TContext>(), cacheOption);
 
 		public virtual Task<TResult> QueryAsync<TContext, TResult, TCacheEntryOptions>(IAsyncCachedQuery<TContext, TCacheEntryOptions, TResult> query, CacheOption cacheOption = CacheOption.Default)
-		{
-			return Invoker.QueryAsync(query, GetContext<TContext>(), cacheOption);
-		}
+			=> Invoker.QueryAsync(query, GetContext<TContext>(), cacheOption);
 
 		public virtual void EvictCachedResult<TCacheEntryOptions>(ISyncCachedQuery<TCacheEntryOptions> query)
-		{
-			Invoker.EvictCachedResult(query);
-		}
+			=> Invoker.EvictCachedResult(query);
 
 		public virtual Task EvictCachedResultAsync<TCacheEntryOptions>(IAsyncCachedQuery<TCacheEntryOptions> query)
-		{
-			return Invoker.EvictCachedResultAsync(query);
-		}
+			=> Invoker.EvictCachedResultAsync(query);
 
 		public virtual void UpdateCachedResult<TCacheEntryOptions>(ISyncCachedQuery<TCacheEntryOptions> executedQuery)
-		{
-			Invoker.UpdateCachedResult(executedQuery);
-		}
+			=> Invoker.UpdateCachedResult(executedQuery);
 
 		public virtual Task UpdateCachedResultAsync<TCacheEntryOptions>(IAsyncCachedQuery<TCacheEntryOptions> executedQuery)
-		{
-			return Invoker.UpdateCachedResultAsync(executedQuery);
-		}
+			=> Invoker.UpdateCachedResultAsync(executedQuery);
 
 		public virtual void Command<TContext>(ISyncCommand<TContext> command)
-		{
-			Invoker.Command(command, GetContext<TContext>());
-		}
+			=> Invoker.Command(command, GetContext<TContext>());
 
 		public virtual Task CommandAsync<TContext>(IAsyncCommand<TContext> command)
-		{
-			return Invoker.CommandAsync(command, GetContext<TContext>());
-		}
+			=> Invoker.CommandAsync(command, GetContext<TContext>());
 
 		public virtual TResult Command<TContext, TResult>(ISyncCommand<TContext, TResult> command)
-		{
-			return Invoker.Command(command, GetContext<TContext>());
-		}
+			=> Invoker.Command(command, GetContext<TContext>());
 
 		public virtual Task<TResult> CommandAsync<TContext, TResult>(IAsyncCommand<TContext, TResult> command)
-		{
-			return Invoker.CommandAsync(command, GetContext<TContext>());
-		}
+			=> Invoker.CommandAsync(command, GetContext<TContext>());
 	}
 }
